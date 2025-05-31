ba3e13f1ea374fe22a76951f3f062698ae10d916
Morteza Gharedaghi <M.Gharedaghi@sadad.co.ir>
2025-05-31 08:52:50 +0330

    Initial Commit

---
+++ Shecan/ShecanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShecanApp.swift
+//  Shecan
+//
+//  Created by Morteza on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct ShecanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
